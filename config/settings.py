5aef774108f35c06b4c497c70cc22da3b1f5a80e
Morphy1 <jefemaestroasf@gmail.com>
2026-07-18 09:05:42 -0500

    agregar logging en settings.py

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,26 @@ LOGIN_REDIRECT_URL = "dashboard"
 
 LOGOUT_REDIRECT_URL = "login"
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
